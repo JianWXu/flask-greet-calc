3b96ed453c510e4afb8f4b39d877b696fc587391
JianWXu <jianwxu88@gmail.com>
2023-05-03 05:02:10 -0700

    finished assignment
    Flask Greet and Calc servers testing all passed

--- calc/app.py
+++ calc/app.py
@@ -1,2 +1,51 @@
-from flask import Flask
-app = Flask(__name__)  # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+app = Flask(__name__)
+
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+
+@app.route('/math/<method>')
+def math_method(method):
+    arg1 = int(request.args.get('a'))
+    arg2 = int(request.args.get('b'))
+    answer = operators[method](arg1, arg2)
+    return f"<h1>The answer is {answer}"
+
+
+@app.route("/add")
+def math_add():
+    arg1 = int(request.args.get('a'))
+    arg2 = int(request.args.get('b'))
+    answer = add(arg1, arg2)
+    return f"<h1>The answer is {answer}"
+
+
+@app.route("/sub")
+def math_sub():
+    arg1 = int(request.args.get('a'))
+    arg2 = int(request.args.get('b'))
+    answer = sub(arg1, arg2)
+    return f"<h1>The answer is {answer}"
+
+
+@app.route("/mult")
+def math_mult():
+    arg1 = int(request.args.get('a'))
+    arg2 = int(request.args.get('b'))
+    answer = mult(arg1, arg2)
+    return f"<h1>The answer is {answer}"
+
+
+@app.route("/div")
+def math_div():
+    arg1 = int(request.args.get('a'))
+    arg2 = int(request.args.get('b'))
+    answer = div(arg1, arg2)
+    return f"<h1>The answer is {answer}"
